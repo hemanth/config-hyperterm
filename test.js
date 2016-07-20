9d2cfb683b0afecdc45083def01d85d162e76016
Hemanth.HM <hemanth.hm@gmail.com>
2016-07-20 13:56:48 +0530

    hyperterm config mock for test

--- test.js
+++ test.js
@@ -2,5 +2,5 @@ import test from 'ava';
 import fn from './';
 
 test('title', t => {
-	t.is(fn().get('plugins')[0], 'hyperterm-snazzy');
+	t.is(fn('./hyperterm.mock.js').get('plugins')[0], 'hyperterm-snazzy');
 });
